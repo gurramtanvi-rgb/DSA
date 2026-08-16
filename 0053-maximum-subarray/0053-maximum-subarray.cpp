59aba0c26eabef164a4ffc321ef4c10a81e749f9
gurramtanvi-rgb <gurramtanvi@gmail.com>
2026-08-16 22:02:36 +0530

    Time: 0 ms (100%), Space: 71.9 MB (15.17%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        
+        int current_sum=0;
+        int max_sum=nums[0];
+        int n=nums.size();
+
+        for(int num:nums){
+          current_sum+=num;
+          max_sum = max(max_sum,current_sum);
+
+          if(current_sum<0){
+            current_sum = 0;
+          }
+            
+        }
+        return max_sum;
+    }
+};
