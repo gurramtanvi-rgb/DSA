6258da90e63a9bf6650585b19030365c8beda7b2
gurramtanvi-rgb <gurramtanvi@gmail.com>
2026-08-28 08:00:38 +0530

    Time: 0 ms (100%), Space: 21.9 MB (38.52%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        
+        int n=nums.size();
+
+        int sum1 = accumulate(nums.begin(),nums.end(),0);
+        int sum2 = (n*(n+1))/2;
+        
+        return sum2-sum1;
+    }
+};
